a924435f8802f2abf903169c1d551897a7c1391b
Cory Fitz <coryfitz@Corys-MBP.cust.communityfibre.co.uk>
2024-04-10 00:49:00 +0100

    readme for pypi

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup, find_packages
 
-with open('README.md', 'r') as f:
-    description = f.read()
+# Read the contents of your README file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='crowbar_package_manager',
     version='0.1.4',
     packages=find_packages(),
-    install_requires=[
-
-    ],
+    install_requires=[],
     entry_points={
         'console_scripts': [
             'crowbar=crowbar:main',
             'cb=crowbar:main',
-        ]
-    }
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This line is important for Markdown rendering on PyPI
 )
